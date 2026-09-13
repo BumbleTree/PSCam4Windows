f347de6a191ba893e01af801c26d26bb1c2bd8d0
Moon36 <manuel-waibel@gmx.at>
2026-09-13 18:19:16 +0200

    Add static path for Visual Studio Build Tools 2022

--- build.bat
+++ build.bat
@@ -27,6 +27,8 @@ if not defined VCVARS_PATH (
         set "VCVARS_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
     ) else if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" (
         set "VCVARS_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    ) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" (
+        set "VCVARS_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
     )
 )
 
